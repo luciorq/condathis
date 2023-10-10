f3960b5e7780cbbe10370287c61e7216ba8159d4
luciorq <luciorqueiroz@gmail.com>
2023-10-10 01:51:41 -0400

    refactor: export env_exists

--- R/utils.R
+++ R/utils.R
@@ -140,7 +140,10 @@ list_packages <- function(env_name = "condathis-env", verbose = FALSE) {
 }
 
 #' Check If Environment Names Already exists
+#'
 #' @inheritParams create_env
+#'
+#' @export
 env_exists <- function(env_name = "condathis-env") {
   available_envs <- list_envs()
   condathis_env_path <- env_name
