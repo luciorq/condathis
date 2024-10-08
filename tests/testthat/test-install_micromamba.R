a2db690fd4491cc75d3cbb85b440a19c4fa570ac
luciorq <luciorqueiroz@gmail.com>
2024-10-08 10:23:16 -0400

    tests: add check_internet tests

--- tests/testthat/test-install_micromamba.R
+++ tests/testthat/test-install_micromamba.R
@@ -1,10 +1,27 @@
 test_that("Micromamba installation", {
   withr::local_path(new = list(), action = "replace")
   umamba_bin_path <- install_micromamba(force = FALSE)
-  expect_true(fs::file_exists(umamba_bin_path))
+  testthat::expect_true(fs::file_exists(umamba_bin_path))
 })
 
 test_that("Micromamba already installed", {
-  umamba_bin_path <- install_micromamba(force = FALSE)
-  expect_true(fs::file_exists(umamba_bin_path))
+  testthat::expect_message(
+    object = {
+      umamba_bin_path <- install_micromamba(force = FALSE)
+    },
+    regexp = "installed"
+  )
+  testthat::expect_true(fs::file_exists(umamba_bin_path))
+})
+
+test_that("Connection not available", {
+  testthat::local_mocked_bindings(check_connection = function(...) {
+    return(FALSE)
+  })
+  testthat::expect_error(
+    object = {
+      umamba_bin_path <- install_micromamba(force = TRUE)
+    },
+    class = "condathis_github_not_reachable"
+  )
 })
