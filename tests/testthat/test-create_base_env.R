cefdeaac5ee9970f7bc0d846687aadb5a3324964
luciorq <luciorqueiroz@gmail.com>
2024-10-17 11:28:15 -0400

    tests: fix regression in test

--- tests/testthat/test-create_base_env.R
+++ tests/testthat/test-create_base_env.R
@@ -1,5 +1,7 @@
 testthat::test_that("Create base env", {
-  fs::dir_delete(fs::path(get_install_dir(), "envs", "condathis-env"))
+  if (fs::dir_exists(fs::path(get_install_dir(), "envs", "condathis-env"))) {
+    fs::dir_delete(fs::path(get_install_dir(), "envs", "condathis-env"))
+  }
   create_base_env()
   testthat::expect_true(fs::file_exists(fs::path(get_install_dir(), "envs", "condathis-env", "conda-meta", "history")))
 })
