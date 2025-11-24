523359a015bd92f309047d20735e9d259d54152a
luciorq <luciorqueiroz@gmail.com>
2025-11-24 17:12:46 -0500

    tests: update channel string parsing

--- tests/testthat/test-format_channels_args.R
+++ tests/testthat/test-format_channels_args.R
@@ -1,8 +1,8 @@
 test_that("Format Channels args", {
-  expect_equal(format_channels_args(), c("-c", "bioconda", "-c", "conda-forge"))
+  expect_equal(format_channels_args(), c("-c", "conda-forge", "-c", "bioconda"))
   expect_equal(
     format_channels_args(NULL),
-    c("-c", "bioconda", "-c", "conda-forge")
+    c("-c", "conda-forge", "-c", "bioconda")
   )
   expect_equal(format_channels_args(NA), c("-c", NA_character_))
   expect_equal(format_channels_args(1), c("-c", "1"))
